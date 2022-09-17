205bafcb26e14681a693ce7d515c8911d20b5b6b
Maxim Mirow <maxim.doerr@icloud.com>
2022-09-17 22:05:23 +0200

    ⚰️ config data

--- config.lua
+++ config.lua
@@ -3,7 +3,6 @@ Config = {}
 Config.Markers = {
     {
         position = vector3(373.9579, 326.7603, 103.5664),
-        radius = 2.5,
         markerRenderingRadius = 10,
         markerType = 1,
         markerColor = {
@@ -11,8 +10,9 @@ Config.Markers = {
             g = 255,
             b = 0
         },
-        markerSize = 0.5,
-        markerText = "Press [E] to open the shop menu",
+        markerSize = 1.0,
+        radius = 1.5,
+        markerText = "Press ~INPUT_PICKUP~ to open the shop menu",
         trigger = function()
             print('Opened Menu')
             TriggerEvent('openShopMenu')
